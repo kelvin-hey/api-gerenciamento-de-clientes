a66c081b297394c7bddf7d827ce09bcec4fe920c
Kelvin Hey <57559711+kelvin-hey@users.noreply.github.com>
2023-10-24 14:22:24 -0300

    ✨ Feature: added new files

--- src/main/java/br/com/zeroes/clientes/controller/HomeController.java
+++ src/main/java/br/com/zeroes/clientes/controller/HomeController.java
@@ -3,7 +3,9 @@ package br.com.zeroes.clientes.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -17,14 +19,34 @@ public class HomeController {
     }
 
     @GetMapping("/mensagem")
-    public String mensagem(ModelMap modelMap) {
-        
-        modelMap.addAttribute("mensagem", "Mensagem de boas vindas do servidor");
-        
-        return "mensagem";
+    public ModelAndView mensagem() {       
+        ModelAndView modelAndView = new ModelAndView();
 
-        // model.addAttribute("mensagem", "Mensagem vinda do servidor!");
-        // return "mensagem";
+        modelAndView.setViewName("mensagem");
+        modelAndView.addObject("mensagem", "Mensagem vinda do servidor");
+
+        return modelAndView;
     }
+
+    @GetMapping("/saudacao/{nome}")
+    public ModelAndView saudacao(@PathVariable String nome) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("saudacao");
+        modelAndView.addObject("nome", nome);
+
+        return modelAndView;
+    }
+
+    // @GetMapping("/mensagem")
+    // public String mensagem(ModelMap modelMap) {
+        
+    //     modelMap.addAttribute("mensagem", "Mensagem de boas vindas do servidor");
+        
+    //     return "mensagem";
+
+    //     // model.addAttribute("mensagem", "Mensagem vinda do servidor!");
+    //     // return "mensagem";
+    // }
 
 }
