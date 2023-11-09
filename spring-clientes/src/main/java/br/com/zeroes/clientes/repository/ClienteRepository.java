433aff3c541291cc157c6449927fac310b67e6b5
Kelvin Hey <57559711+kelvin-hey@users.noreply.github.com>
2023-11-09 17:32:41 -0300

    Delete spring-clientes directory

--- spring-clientes/src/main/java/br/com/zeroes/clientes/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.zeroes.clientes.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.zeroes.clientes.model.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-    
-}
